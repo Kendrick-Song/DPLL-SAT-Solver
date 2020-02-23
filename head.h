4921bffaf389b019eda9f516dbff18cb97dd05d9
Kendrick Song <kendrick818@outlook.com>
2020-02-24 00:13:16 +0800

    Create head.h

---
+++ head.h
@@ -0,0 +1,24 @@
+/**
+ * 文件名称：head.h
+ * 文件描述：SAT求解器相关常量，数据结构定义，相关操作函数声明
+**/
+#ifndef _HEAD_H
+#define _HEAD_H
+
+/*头文件引用*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+/*常量定义*/
+typedef int status;
+#define TRUE 1
+#define FALSE 0
+
+/*数据结构定义*/
+
+/*函数声明*/
+
+#endif
